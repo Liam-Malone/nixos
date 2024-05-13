7ad0c550d28a776baa79e0d2dd5cbd46aac7b7d5
Liam Malone <maloneliam@proton.me>
2024-05-13 15:10:59 +0100

    tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -3,18 +3,33 @@
 {
   programs.tmux = {
     enable = true;
-    shell = "${pkgs.bash}/bin/bash";
     clock24 = true;
     terminal = "tmux-256color";
     plugins = with pkgs; [
       tmuxPlugins.sensible
       {
-        plugin = tmuxPlugins.catpuccin;
+        plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
           set -g @catpuccin_flavour 'frappe'
           set -g @catpuccin_date_time "%H:%M"
         '';
       }
     ];
+    extraConfig = ''
+      # DESIGN TWEAKS
+      
+      # don't do anything when a 'bell' rings
+      set -g visual-activity off
+      set -g visual-bell off
+      set -g visual-silence off
+      setw -g monitor-activity off
+      set -g bell-action none
+      
+      # copy mode
+      setw -g mode-style 'fg=colour1 bg=colour18 bold'
+     
+      # messages
+      set -g message-style 'fg=colour2 bg=colour0 bold'
+    '';
   };
 }
