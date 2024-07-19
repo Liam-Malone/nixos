083805e0413280382a86fc711a74b3b98b95c15e
Liam Malone <maloneliam@proton.me>
2024-07-19 16:23:01 -0400

    add fastfetch.nix

---
+++ modules/home-manager/fastfetch.nix
@@ -0,0 +1,53 @@
+{ pkgs, ... }:
+
+{
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "~/pictures/smol-penguin.png";
+        type = "kitty-direct";
+        width = 18;
+        height = 9;
+        padding = { 
+          top = 2;
+          left = 1;
+          right = 3;
+        };
+      };
+      display = {
+        separator = " -> ";
+      };
+      modules = [
+        "title"
+        {
+          type = "custom";
+          format = "────────── Env ──────────";
+        }
+        {
+          type = "os";
+          key = "OS ";
+        }
+        {
+          type = "wm";
+          key = "WM ";
+        }
+        "editor"
+        "break"
+        {
+          type = "custom";
+          format = "────────── Sys ──────────";
+        }
+        "cpu"
+        "gpu"
+        {
+          type = "memory";
+          key = "MEM";
+        }
+        "vulkan"
+        "disk"
+        "battery"
+      ];
+    };
+  };
+}
