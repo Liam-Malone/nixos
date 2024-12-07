4da67e9f0ffc30b4e2a3e367710d82ab5a9ae9c6
Liam Malone <maloneliam@proton.me>
2024-12-07 14:50:20 +0100

    default to setting colorscheme to tokyonight-night, no transparency

--- modules/non-nix_configs/nvim/after/plugin/colors.lua
+++ modules/non-nix_configs/nvim/after/plugin/colors.lua
@@ -6,3 +6,5 @@ function ColorMyTerminal(color)
     vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
     vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
+
+vim.cmd.colorscheme("tokyonight-night")
